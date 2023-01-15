76186b9e1f95e9bb15b47db6e130f1fb96b7657a
GenerelSchwerz <xXSG.KingXx@gmail.com>
2023-01-15 12:10:22 -0500

    updated tests and moved them out of src

--- tests/fakeWorld.ts
+++ tests/fakeWorld.ts
@@ -1,16 +1,16 @@
 import type { Entity } from "prismarine-entity";
 import type { Effect } from "mineflayer";
-import { EntityPhysics } from "../physics/engines";
-import { EntityState } from "../physics/states";
-import { EPhysicsCtx, PhysicsSettings } from "../physics/settings";
-import { applyMdToNewEntity } from "../util/physicsUtils";
-import { ControlStateHandler } from "../physics/player";
+import { EntityPhysics } from "../src/physics/engines";
+import { EntityState } from "../src/physics/states";
+import { EPhysicsCtx, PhysicsSettings } from "../src/physics/settings";
+import { applyMdToNewEntity } from "../src/util/physicsUtils";
+import { ControlStateHandler } from "../src/physics/player";
 import { Vec3 } from "vec3";
 import md from "minecraft-data";
 import block from "prismarine-block";
 import expect from "expect";
 
-import { initSetup } from "../index";
+import { initSetup } from "../src/index";
 
 const mcData = md("1.17.1");
 const Block = (block as any)("1.17.1");
@@ -70,20 +70,26 @@ playerState.controlState.forward = true;
 // simulate until on ground.
 while (!playerCtx.state.onGround) {
     physics.simulate(playerCtx, fakeWorld).applyToBot(fakePlayer as any); // (applyToBot since fakePlayer is supposed to be a bot)
-    console.log(fakePlayer.entity.position, fakePlayer.entity.velocity);
+    // console.log(fakePlayer.entity.position, fakePlayer.entity.velocity);
+}
+
+if (playerState.controlState.forward) {
+    expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, -3.4508449226731694)); // it works.
+} else {
+    expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, 0)); // it works.
 }
 
 playerCtx.state.controlState.set("jump", true);
 for (let i = 0; i < 12; i++) {
     physics.simulate(playerCtx, fakeWorld).applyToBot(fakePlayer as any); // (applyToBot since fakePlayer is supposed to be a bot)
-    console.log(fakePlayer.entity.position, fakePlayer.entity.velocity);
+    // console.log(fakePlayer.entity.position, fakePlayer.entity.velocity);
 }
 
 if (playerState.controlState.forward) {
-    // expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, -3.4508449226731694)); // it works.
+    expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, -5.788782872583908)); // it works.
 } else {
-    // expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, 0)); // it works.
-
+    expect(fakePlayer.entity.position).toEqual(new Vec3(0, 60, 0)); // it works.
 }
+
 
 console.log(fakePlayer.entity.position); //manual run.
