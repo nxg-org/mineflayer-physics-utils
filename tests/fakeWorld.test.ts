37f8ed66b0a43b8378b258738b630aee27fe8f71
GenerelSchwerz <xXSG.KingXx@gmail.com>
2025-01-28 14:28:22 -0500

    better fakeWorld

--- tests/fakeWorld.test.ts
+++ tests/fakeWorld.test.ts
@@ -2,7 +2,7 @@ import { describe, it, beforeEach } from "mocha";
 import expect from "expect";
 import { Vec3 } from "vec3";
 import md from "minecraft-data";
-import block from "prismarine-block";
+import block, {Block as PBlock} from "prismarine-block";
 import { applyMdToNewEntity } from "../src/util/physicsUtils";
 import { EPhysicsCtx, PhysicsWorldSettings } from "../src/physics/settings";
 import { ControlStateHandler } from "../src/physics/player";
@@ -13,19 +13,35 @@ import { Bot, ControlState } from "mineflayer";
 
 const version = "1.12.2";
 const mcData = md(version);
-const Block = (block as any)(version);
+const Block = (block)(version) as typeof PBlock;
 
 const groundLevel = 4;
 const floatingOffset = 5;
 const control: {[key: string]: boolean} = {};
 
-const fakeWorld = {
-  getBlock: (pos: Vec3) => {
+class FakeWorld {
+
+  overrideBlocks: {[key: string]: PBlock} = {}
+
+  setOverrideBlock(pos: Vec3, type: number) {
+    this.overrideBlocks[`${pos.x},${pos.y},${pos.z}`] = new Block(type, 0, 0);
+  }
+
+  clearOverrides() {
+    this.overrideBlocks = {};
+  }
+  
+  getBlock(pos: Vec3) {
+    const key = `${pos.x},${pos.y},${pos.z}`;
+    if (this.overrideBlocks[key]) {
+      return this.overrideBlocks[key];
+    }
+
     const type = pos.y < groundLevel ? mcData.blocksByName.stone.id : mcData.blocksByName.air.id;
     const b = new Block(type, 0, 0);
     b.position = pos;
     return b;
-  },
+  }
 };
 
 function createFakePlayer(pos: Vec3, groundLevel: number) {
@@ -64,21 +80,25 @@ initSetup(mcData);
 const playerType = mcData.entitiesByName["player"];
 
 describe("Physics Simulation Tests", () => {
-  let fakePlayer: ReturnType<typeof createFakePlayer>;
+  let fakePlayer: ReturnType<typeof createFakePlayer> | any;
   let physics: IPhysics;
   let playerCtx: EPhysicsCtx;
   let playerState: PlayerState;
+  const fakeWorld = new FakeWorld();
 
   const setupEntity = (yOffset: number) => {
     fakePlayer = createFakePlayer(new Vec3(0, groundLevel + yOffset, 0), groundLevel);
-    fakePlayer.entity = applyMdToNewEntity(EPhysicsCtx, playerType, fakePlayer.entity) as any;
-    physics = new BotcraftPhysics(mcData);
-    playerCtx = EPhysicsCtx.FROM_BOT(physics, fakePlayer as any);
+    fakePlayer.entity = applyMdToNewEntity(EPhysicsCtx, playerType, fakePlayer.entity);
+    physics = new EntityPhysics(mcData);
+    playerCtx = EPhysicsCtx.FROM_BOT(physics, fakePlayer );
     playerState = playerCtx.state as PlayerState;
     playerState.control = ControlStateHandler.DEFAULT();
   }
 
 
+  afterEach(() => {
+    fakeWorld.clearOverrides();
+  });
 
   it("should maintain position when gravity is zero", () => {
     setupEntity(floatingOffset);
@@ -87,10 +107,9 @@ describe("Physics Simulation Tests", () => {
     playerCtx.gravity = 0;
 
     for (let i = 0; i < floatingOffset; i++) {
-      console.log(playerState.pos, playerState.vel, playerState.onGround);
-      // playerState.update(fakePlayer);
+      playerState.update(fakePlayer);
       physics.simulate(playerCtx, fakeWorld);
-      playerState.apply(fakePlayer as any);
+      playerState.apply(fakePlayer );
      
     }
 
@@ -104,8 +123,7 @@ describe("Physics Simulation Tests", () => {
 
     for (let i = 0; i < 10; i++) {
       physics.simulate(playerCtx, fakeWorld);
-      playerState.apply(fakePlayer as any);
-      console.log(playerState.pos, playerState.vel);
+      playerState.apply(fakePlayer );
     }
 
     if (playerState.control.forward) {
@@ -124,17 +142,35 @@ describe("Physics Simulation Tests", () => {
 
     for (let i = 0; i < 5; i++) {
       physics.simulate(playerCtx, fakeWorld);
-      playerState.apply(fakePlayer as any);
+      playerState.apply(fakePlayer );
     }
     expect(fakePlayer.entity.position).toEqual(new Vec3(0, groundLevel + floatingOffset, 0));
     playerCtx.gravity = orgGravity;
 
     while (!fakePlayer.entity.onGround) {
       physics.simulate(playerCtx, fakeWorld);
-      playerState.apply(fakePlayer as any);
-      
+      playerState.apply(fakePlayer );
     }
 
     expect(fakePlayer.entity.position.y).toEqual(groundLevel); // Verify movement in Z direction
+  });
+
+  it("should jump and fall correctly", () => {
+    setupEntity(0);
+    playerState.control.jump = true;
+
+    for (let i = 0; i < 3; i++) {
+      physics.simulate(playerCtx, fakeWorld);
+      playerState.apply(fakePlayer);
+    }
+
+    expect(fakePlayer.entity.position.y).toEqual(5.001335979112147);
+
+    for (let i = 0; i < 9; i++) {
+      physics.simulate(playerCtx, fakeWorld);
+      playerState.apply(fakePlayer);
+    }
+
+    expect(fakePlayer.entity.position.y).toEqual(groundLevel);
   });
 });
